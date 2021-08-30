abdfa5603abc0caa93074302740b8b32179d9b51
K1ndum <57758788+K1ndum@users.noreply.github.com>
2021-08-30 16:36:26 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let queue = [];
+  const brackets = {};
+  const arr = str.split("");
+
+  bracketsConfig.forEach((a) => (brackets[a[0]] = a[1]));
+
+  for (let i = 0; i < arr.length; i++) {
+      if (brackets[queue[queue.length - 1]] === arr[i]) {
+          queue.pop();
+      } else if (brackets.hasOwnProperty(arr[i])) {
+          queue.push(arr[i]);
+      } else {
+          return false;
+      }
+  }
+
+  if (queue.length == 0) {
+      return true;
+  } else {
+      return false;
+  }
 }
